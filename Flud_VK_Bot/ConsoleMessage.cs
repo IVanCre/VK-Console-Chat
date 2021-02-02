74454a1f87d2ff866b9665534cf965f10028592c
IVanCre <75716826+IVanCre@users.noreply.github.com>
2021-02-02 23:21:42 +0700

    Add files via upload

---
+++ Flud_VK_Bot/ConsoleMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flud_VK_Bot
+{
+    class ConsoleMessage
+    {
+        private string Message="";
+
+        internal string Go()
+        {
+            Console.WriteLine("\n Введите свое сообщение и нажмите Enter\n");
+            Message = Console.ReadLine();
+            return Message;
+        }
+    }
+}
